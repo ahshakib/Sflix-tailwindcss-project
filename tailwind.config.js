8dbdef6f0590154ab99e27fa6de3b3c19ce54a19
ahshakib <ahshakib75@gmail.com>
2023-07-12 22:51:41 +0600

    All files are added

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*html'],
+  theme: {
+    extend: {
+      colors: {
+        "dark": "rgb(0 0 0)",
+        "text": "white",
+        "primary": "#E50914",
+        "secondary": "red"
+      }
+    },
+  },
+  plugins: [],
+}
+
